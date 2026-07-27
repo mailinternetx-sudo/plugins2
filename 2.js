0d48607ef892591f8471f192f627f06adfffa064
mailinternetx-sudo <mail.internetx@gmail.com>
2026-07-27 15:22:28 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -367,7 +367,7 @@
             '95.67.104.126:43871',
             '178.150.115.242:8090',
             '95.165.134.227:8090',
-            'ts.maxvol.pro:8090'
+            'https://ts.maxvol.pro/'
         ];
 
         var STORAGE_PRIMARY = 'torrserver_url';
